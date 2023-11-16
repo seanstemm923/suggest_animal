b2ee237b9a4304258b4303720fa1a497694b899e
derpydurpy <stemm003@cougars.csusm.edu>
2023-11-15 21:39:55 -0800

    non-syntax error
    uploaded an image of non-syntax erorr, did not give me a list of any animals, re-prompted.

--- project.pl
+++ project.pl
@@ -1,9 +1,142 @@
-% Pet facts
-pet(dog, mammal, medium, average_lifespan(10), outdoor, expensive, high, moderate, high).
-pet(cat, mammal, small, average_lifespan(13), indoor, cheap, moderate, low, moderate).
-pet(goldfish, fish, small, average_lifespan(5), indoor, cheap, low, low, low).
-pet(parrot, bird, small, average_lifespan(20), indoor, expensive, high, moderate, moderate).
-pet(turtle, reptile, small, average_lifespan(40), indoor, cheap, low, low, moderate).
+/* Amphibians */
+
+pet(african_dwarf_frog, amphibian, small, average_lifespan(6), aquarium, low, moderate, moderate, low).
+pet(american_bullfrog, amphibian, large, average_lifespan(8), pond_or_large_aquarium, moderate, moderate, moderate, high).
+pet(axolotl, amphibian, medium, average_lifespan(12), aquarium, moderate, low, moderate, low).
+pet(red_eyed_tree_frog, amphibian, small, average_lifespan(4), terrarium, low, moderate, moderate, low).
+pet(pacific_tree_frog, amphibian, small, average_lifespan(7), terrarium, low, moderate, moderate, moderate).
+pet(oriental_fire_bellied_toad, amphibian, small, average_lifespan(13), terrarium_with_water, low, moderate, moderate, moderate).
+pet(blue_poison_dart_frog, amphibian, small, average_lifespan(11), terrarium, moderate, moderate, high, low).
+pet(tomato_frog, amphibian, medium, average_lifespan(7), terrarium, low, low, moderate, low).
+pet(leopard_frog, amphibian, medium, average_lifespan(6), aquarium_or_pond, low, moderate, moderate, moderate).
+pet(spotted_salamander, amphibian, small, average_lifespan(25), terrarium, low, low, moderate, low).
+pet(tiger_salamander, amphibian, large, average_lifespan(12), terrarium, low, low, moderate, low).
+pet(green_tree_frog, amphibian, small, average_lifespan(5), terrarium, low, moderate, moderate, low).
+pet(horned_frog, amphibian, medium, average_lifespan(8), terrarium, moderate, low, moderate, low).
+pet(siren, amphibian, large, average_lifespan(25), large_aquarium, moderate, low, moderate, low).
+pet(newt, amphibian, small, average_lifespan(10), aquarium_or_terrarium, low, moderate, moderate, low).
+pet(barking_tree_frog, amphibian, medium, average_lifespan(9), terrarium, low, moderate, moderate, moderate).
+pet(amazon_milk_frog, amphibian, medium, average_lifespan(9), terrarium, moderate, moderate, moderate, low).
+pet(glass_frog, amphibian, small, average_lifespan(12), terrarium, moderate, moderate, moderate, low).
+pet(surinam_toad, amphibian, medium, average_lifespan(8), aquarium, moderate, low, moderate, low).
+pet(budgetts_frog, amphibian, medium, average_lifespan(6), terrarium, moderate, moderate, moderate, moderate).
+
+/* Fish */
+
+pet(goldfish, fish, small_to_medium, average_lifespan(10), aquarium, moderate, moderate, moderate, low).
+pet(betta_fish, fish, small, average_lifespan(3), aquarium, low, low, moderate, low).
+pet(neon_tetra, fish, small, average_lifespan(5), aquarium, low, moderate, low, low).
+pet(guppy, fish, small, average_lifespan(2), aquarium, low, moderate, low, low).
+pet(angelfish, fish, medium, average_lifespan(10), aquarium, moderate, moderate, moderate, low).
+pet(clownfish, fish, small_to_medium, average_lifespan(6), saltwater_aquarium, high, moderate, high, low).
+pet(zebrafish, fish, small, average_lifespan(5), aquarium, low, moderate, low, low).
+pet(oscar, fish, large, average_lifespan(12), large_aquarium, high, moderate, high, low).
+pet(mollies, fish, small, average_lifespan(5), aquarium, low, moderate, low, low).
+pet(swordtail, fish, small, average_lifespan(4), aquarium, low, moderate, low, low).
+pet(discus, fish, medium, average_lifespan(10), large_aquarium, high, moderate, high, low).
+pet(koi, fish, large, average_lifespan(30), pond, high, low, moderate, low).
+pet(plecostomus, fish, medium_to_large, average_lifespan(10), aquarium, moderate, low, moderate, low).
+pet(rainbowfish, fish, medium, average_lifespan(5), aquarium, moderate, moderate, moderate, low).
+pet(cichlids, fish, medium, average_lifespan(8), aquarium, moderate, high, high, moderate).
+pet(siamese_algae_eater, fish, small, average_lifespan(10), aquarium, low, moderate, low, low).
+pet(cherry_barb, fish, small, average_lifespan(5), aquarium, low, moderate, low, low).
+pet(harlequin_rasbora, fish, small, average_lifespan(6), aquarium, low, moderate, low, low).
+pet(green_tiger_barb, fish, small, average_lifespan(7), aquarium, low, high, moderate, low).
+pet(black_molly, fish, small, average_lifespan(5), aquarium, low, moderate, moderate, low).
+
+
+/*Mammals*/
+
+	/*Types of dogs*/
+
+pet(labrador_retriever, mammal, medium, average_lifespan(12), indoor_outdoor, high, high, moderate, moderate).
+pet(german_shepherd, mammal, large, average_lifespan(11), indoor_outdoor, high, high, high, high).
+pet(golden_retriever, mammal, large, average_lifespan(12), indoor_outdoor, high, high, moderate, moderate).
+pet(french_bulldog, mammal, small, average_lifespan(11), indoor, moderate, low, moderate, low).
+pet(beagle, mammal, medium, average_lifespan(12), indoor_outdoor, moderate, high, moderate, high).
+pet(bulldog, mammal, medium, average_lifespan(8), indoor, high, low, high, moderate).
+pet(poodle, mammal, medium, average_lifespan(12), indoor_outdoor, high, high, high, low).
+pet(rottweiler, mammal, large, average_lifespan(10), indoor_outdoor, high, high, high, high).
+pet(yorkshire_terrier, mammal, small, average_lifespan(13), indoor, moderate, moderate, high, moderate).
+pet(boxer, mammal, large, average_lifespan(10), indoor_outdoor, high, high, moderate, moderate).
+pet(dachshund, mammal, small, average_lifespan(12), indoor, moderate, moderate, moderate, moderate).
+pet(siberian_husky, mammal, large, average_lifespan(12), indoor_outdoor, high, high, high, high).
+pet(great_dane, mammal, large, average_lifespan(8), indoor_outdoor, high, moderate, moderate, high).
+pet(doberman_pinscher, mammal, large, average_lifespan(11), indoor_outdoor, high, high, high, high).
+pet(shih_tzu, mammal, small, average_lifespan(13), indoor, low, moderate, high, low).
+pet(australian_shepherd, mammal, medium, average_lifespan(13), indoor_outdoor, high, high, moderate, moderate).
+pet(miniature_schnauzer, mammal, small, average_lifespan(12), indoor, moderate, high, moderate, moderate).
+pet(cavalier_king_charles_spaniel, mammal, small, average_lifespan(12), indoor, moderate, moderate, moderate, low).
+pet(shetland_sheepdog, mammal, small, average_lifespan(13), indoor_outdoor, moderate, high, moderate, moderate).
+pet(bernese_mountain_dog, mammal, large, average_lifespan(8), indoor_outdoor, high, moderate, high, low).
+
+	/* Types of Cats */
+
+pet(siamese_cat, mammal, medium, average_lifespan(15), indoor, moderate, high, moderate, high).
+pet(persian_cat, mammal, medium, average_lifespan(14), indoor, high, low, high, low).
+pet(maine_coon, mammal, large, average_lifespan(13), indoor, high, moderate, moderate, low).
+pet(ragdoll, mammal, large, average_lifespan(15), indoor, moderate, low, moderate, low).
+pet(bengal_cat, mammal, medium, average_lifespan(14), indoor, high, high, moderate, high).
+pet(sphynx_cat, mammal, medium, average_lifespan(13), indoor, moderate, high, high, low).
+pet(british_shorthair, mammal, medium, average_lifespan(14), indoor, moderate, moderate, low, low).
+pet(abyssinian_cat, mammal, medium, average_lifespan(15), indoor, moderate, high, moderate, moderate).
+pet(birman, mammal, medium, average_lifespan(14), indoor, moderate, low, moderate, low).
+pet(oriental_shorthair, mammal, medium, average_lifespan(15), indoor, moderate, high, moderate, moderate).
+pet(american_shorthair, mammal, medium, average_lifespan(15), indoor, moderate, moderate, moderate, low).
+pet(scottish_fold, mammal, medium, average_lifespan(15), indoor, moderate, low, moderate, low).
+pet(norwegian_forest_cat, mammal, large, average_lifespan(14), indoor, high, moderate, moderate, low).
+pet(siberian_cat, mammal, large, average_lifespan(15), indoor, high, moderate, moderate, low).
+pet(russian_blue, mammal, medium, average_lifespan(15), indoor, moderate, low, low, low).
+pet(egyptian_mau, mammal, medium, average_lifespan(15), indoor, moderate, high, moderate, low).
+pet(exotic_shorthair, mammal, medium, average_lifespan(15), indoor, moderate, low, moderate, low).
+pet(himalayan_cat, mammal, medium, average_lifespan(15), indoor, high, low, high, low).
+pet(burmese_cat, mammal, medium, average_lifespan(18), indoor, moderate, high, moderate, low).
+pet(ragamuffin, mammal, large, average_lifespan(16), indoor, moderate, low, moderate, low).
+
+/*other mammals*/
+
+pet(guinea_pig, mammal, small, average_lifespan(6), cage, low, moderate, moderate, low).
+pet(rabbit, mammal, medium, average_lifespan(9), indoor_cage_with_play_area, moderate, high, high, low).
+pet(hamster, mammal, small, average_lifespan(3), cage, low, moderate, low, low).
+pet(ferret, mammal, medium, average_lifespan(8), large_cage_with_play_area, moderate, high, high, moderate).
+pet(chinchilla, mammal, small, average_lifespan(12), cage, moderate, moderate, high, low).
+pet(rat, mammal, small, average_lifespan(3), cage, low, high, moderate, low).
+pet(mouse, mammal, small, average_lifespan(2), cage, low, moderate, low, low).
+pet(hedgehog, mammal, small, average_lifespan(5), cage, moderate, moderate, moderate, low).
+pet(squirrel, mammal, small, average_lifespan(8), large_cage_with_play_area, moderate, high, high, moderate).
+pet(sugar_glider, mammal, small, average_lifespan(12), large_cage_with_play_area, moderate, high, high, moderate).
+pet(miniature_pig, mammal, large, average_lifespan(15), indoor_outdoor, high, moderate, high, moderate).
+pet(gerbil, mammal, small, average_lifespan(4), cage, low, moderate, low, low).
+pet(capybara, mammal, large, average_lifespan(10), large_outdoor_space, high, moderate, high, low).
+pet(degu, mammal, small, average_lifespan(7), cage, low, high, moderate, low).
+pet(skunk, mammal, medium, average_lifespan(10), indoor_outdoor, high, moderate, high, moderate).
+pet(wallaby, mammal, large, average_lifespan(12), large_outdoor_space, high, high, high, moderate).
+pet(pot_bellied_pig, mammal, large, average_lifespan(14), indoor_outdoor, high, moderate, high, moderate).
+pet(prairie_dog, mammal, small, average_lifespan(8), large_cage_with_play_area, moderate, high, high, moderate).
+pet(flying_squirrel, mammal, small, average_lifespan(6), cage, moderate, high, moderate, low).
+
+/*Reptiles*/
+
+pet(leopard_gecko, reptile, small, average_lifespan(14), terrarium, low, low, moderate, low).
+pet(bearded_dragon, reptile, medium, average_lifespan(12), terrarium, moderate, moderate, high, moderate).
+pet(corn_snake, reptile, medium, average_lifespan(15), terrarium, moderate, low, moderate, low).
+pet(ball_python, reptile, medium, average_lifespan(20), terrarium, moderate, low, moderate, low).
+pet(russian_tortoise, reptile, medium, average_lifespan(40), terrarium_with_outdoor_access, moderate, low, high, low).
+pet(red_eared_slider_turtle, reptile, medium, average_lifespan(25), aquarium_with_land_area, moderate, moderate, high, low).
+pet(crested_gecko, reptile, small, average_lifespan(15), terrarium, moderate, moderate, moderate, low).
+pet(green_iguana, reptile, large, average_lifespan(20), large_terrarium_with_climbing_area, high, moderate, high, moderate).
+pet(blue_tongued_skink, reptile, medium, average_lifespan(18), terrarium, moderate, moderate, moderate, low).
+pet(boa_constrictor, reptile, large, average_lifespan(20), large_terrarium, high, low, high, low).
+pet(chameleon, reptile, small, average_lifespan(7), terrarium, high, moderate, high, low).
+pet(king_snake, reptile, medium, average_lifespan(15), terrarium, moderate, low, moderate, low).
+pet(milk_snake, reptile, small, average_lifespan(12), terrarium, low, low, moderate, low).
+pet(anole, reptile, small, average_lifespan(5), terrarium, low, high, moderate, low).
+pet(monitor_lizard, reptile, large, average_lifespan(15), large_terrarium_with_outdoor_access, high, high, high, moderate).
+pet(turtle, reptile, medium, average_lifespan(40), indoor, cheap, low, low, moderate).
+pet(gecko, reptile, small, average_lifespan(10), terrarium, low, moderate, moderate, low).
+pet(snapping_turtle, reptile, large, average_lifespan(30), large_aquarium_with_land_area, high, low, high, low).
+pet(tegu, reptile, large, average_lifespan(15), large_terrarium_with_outdoor_access, high, high, high, moderate).
+pet(uromastyx, reptile, medium, average_lifespan(15), terrarium, moderate, low, moderate, low).
 
 % Recommendation rule
 recommend_pet(Classification, Size, UserMinLifespan, Livingspace, UserBudget, Activity, Noise, Maintenance, Pet) :-
